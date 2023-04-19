875285d740f6fe82f57f81a29d2ee92ef708850c
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-04-19 14:29:54 +0100

    recursion range of numbers working

--- recursion-range-of-numbers.js
+++ recursion-range-of-numbers.js
@@ -7,11 +7,23 @@ function rangeOfNumbersForLoop(low, high) {
     return arr;
 }
 
+function rangeOfNumbersRecursion(low, high) {
+    // Base
+    if (high < low) {
+        return [];
+    }
+    // Iterate
+    const arr = rangeOfNumbersRecursion(low, high-1);
+    // Body
+    arr.push(high);
+    return arr;
+}
 
-// console.log("---------- recursion");
-// console.count(rangeOfNumbers(1, 5)); // [1,2,3,4,5]
-// console.count(rangeOfNumbers(6, 9)); // [6,7,8,9]
-// console.count(rangeOfNumbers(4, 4)); // [4]
+
+console.log("---------- recursion");
+console.log(rangeOfNumbersRecursion(1, 5)); // [1,2,3,4,5]
+console.log(rangeOfNumbersRecursion(6, 9)); // [6,7,8,9]
+console.log(rangeOfNumbersRecursion(4, 4)); // [4]
 console.log("---------- for loop");
 console.log(rangeOfNumbersForLoop(1, 5)); // [1,2,3,4,5]
 console.log(rangeOfNumbersForLoop(6, 9)); // [6,7,8,9]
