984730f272fed3f628c30d2f99c24694c02b3245
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-04-21 15:11:25 +0100

    adding template-literals.js, prototype-based-oop-es5.js and w3schools-objects.js.

---
+++ template-literals.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+function makeList() {
+    // Only change code below this line
+    
+    const failureItems = [];
+    
+    // OBJs = OBJs = SAME
+    let failurePropertyRef = result["failure"];
+    for (let i=0; i<failurePropertyRef.length; i++){
+        console.log(failurePropertyRef[i]);
+        let theStr = `<li class="text-warning">${failurePropertyRef[i]}</li>`;
+        failureItems.push(theStr);
+    }
+
+
+    // Only change code above this line
+
+    return failureItems;
+}
+
+const failuresList = makeList();
+console.log(failuresList);
---
+++ w3schools-objects.js
@@ -0,0 +1,24 @@
+// Constructor function for Person objects
+// Note: vscode says that the Constructor Function may be converted to a class declaration
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+    this.name = function () {
+        return this.firstName+" "+this.lastName;
+    };
+}
+// Create a Person object
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+console.log("My father is " + myFather.age + "."); 
+console.log("My mother is " + myMother.age + "."); 
+console.log("--------------------------");
+// Adding a property to an existing object is easy
+myFather.natioanlity = "English";
+console.log("My father is " + myFather.natioanlity + "."); 
+console.log("My father's full name is "+ myFather.name());
+
+
+
